3f3fa8ac572b121b1e083fef81a7c3f3f2e6041e
Roberto Entringer <r.entringer@gmail.com>
2019-05-28 14:57:18 +0200

    Edit eslint rules for better code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": ["error", { semi: false, printWidth: 100 }],
+    "comma-dangle": ["error", "never"]
   },
   parserOptions: {
     parser: "babel-eslint"
   },
-  "plugins": ["html"]
-};
+  plugins: ["html"]
+}
