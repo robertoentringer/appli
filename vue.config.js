95c85a8fdcb1ba5186735560c1ab5b0f151449a7
Roberto Entringer <r.entringer@gmail.com>
2019-05-28 15:02:57 +0200

    Add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const isProd = process.env.NODE_ENV === "production"
+const TerserPlugin = require("terser-webpack-plugin")
+module.exports = {
+  publicPath: isProd ? "./" : "/",
+  configureWebpack: {
+    devtool: isProd ? "none" : "source-map",
+    optimization: {
+      minimize: isProd,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true
+            }
+          }
+        })
+      ]
+    }
+  }
+}
