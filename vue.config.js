14fae5804e256f436f8323f7667a34f5f4e19a4f
Roberto Enringer <r.entringer@gmail.com>
2020-09-26 10:23:35 +0200

    devServer check cert exists

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,23 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const TerserPlugin = require('terser-webpack-plugin')
 
+const devServer = () =>
+  !isProd &&
+  'LOCALHOST_SSL_KEY' in process.env &&
+  'LOCALHOST_SSL_CRT' in process.env &&
+  fs.existsSync(process.env.LOCALHOST_SSL_KEY) &&
+  fs.existsSync(process.env.LOCALHOST_SSL_CRT)
+    ? {
+        host: 'localhost',
+        https: {
+          key: fs.readFileSync(process.env.LOCALHOST_SSL_KEY),
+          cert: fs.readFileSync(process.env.LOCALHOST_SSL_CRT)
+        }
+      }
+    : ''
+
 module.exports = {
-  devServer: {
-    hot: true,
-    host: 'localhost',
-    https: {
-      key: 'LOCALHOST_SSL_KEY' in process.env && fs.readFileSync(process.env.LOCALHOST_SSL_KEY),
-      cert: 'LOCALHOST_SSL_KEY' in process.env && fs.readFileSync(process.env.LOCALHOST_SSL_CRT)
-    }
-  },
+  ...(devServer() && { devServer: devServer() }),
   publicPath: isProd ? './' : '/',
   configureWebpack: {
     devtool: isProd ? 'none' : 'source-map',
