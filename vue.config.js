c848962b2b781e90bb194c7edeb74cf153f0e7ab
Roberto Enringer <r.entringer@gmail.com>
2020-09-26 09:51:22 +0200

    run dev serve with https

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+const fs = require('fs')
+
 const isProd = process.env.NODE_ENV === 'production'
+
 const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
+  devServer: {
+    hot: true,
+    host: 'localhost',
+    https: {
+      key: 'LOCALHOST_SSL_KEY' in process.env && fs.readFileSync(process.env.LOCALHOST_SSL_KEY),
+      cert: 'LOCALHOST_SSL_KEY' in process.env && fs.readFileSync(process.env.LOCALHOST_SSL_CRT)
+    }
+  },
   publicPath: isProd ? './' : '/',
   configureWebpack: {
     devtool: isProd ? 'none' : 'source-map',
